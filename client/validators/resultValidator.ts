d9314cce4283154a37f31051fc98a41b07930660
Jesse Obinna <jesseobinna7@gmail.com>
2025-11-20 17:05:17 +0100

    updated validator

--- client/validators/resultValidator.ts
+++ client/validators/resultValidator.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const generateResultSchema = z.object({
   session: z.string().min(3, 'session must have at least 3 characters'),
   term: z.string().min(3, 'term must have at least minimum of 3 characters'),
-  level: z.string().min(3, 'level must have at least 3 characters'),
+  level: z.string().min(2, 'level must have at least 2 characters'),
 });
 
 export const generatePositionSchema = z.object({
