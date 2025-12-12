2a912ab78e1a7a26f12f2ecf17b5a7a1cd27afb9
Jesse Obinna <jesseobinna7@gmail.com>
2025-12-12 10:29:32 +0100

    updated result text

--- client/components/shared/results/result-header.tsx
+++ client/components/shared/results/result-header.tsx
@@ -14,42 +14,48 @@ const ResultHeader = ({ result }: { result: StudentResult }) => {
   
 
   return (
-    <Card className='p-4 mb-6 shadow-md'>
-      <CardContent className='grid md:grid-cols-4 gap-4 text-sm text-zinc-800 dark:text-zinc-100'>
-        <div className='flex items-center gap-2'>
-          <User className='w-4 h-4 text-purple-600' />
+    <Card className="p-4 mb-6 shadow-md">
+      <CardContent className="grid md:grid-cols-4 gap-4 text-sm text-zinc-800 dark:text-zinc-100">
+        <div className="flex items-center gap-2">
+          <User className="w-4 h-4 text-purple-600" />
           <span>
             <strong>Name:</strong> {result.firstName} {result.lastName}
           </span>
         </div>
-        <div className='flex items-center gap-2'>
-          <GraduationCap className='w-4 h-4 text-purple-600' />
+        <div className="flex items-center gap-2">
+          <GraduationCap className="w-4 h-4 text-purple-600" />
           <span>
             <strong>Level:</strong> {result.level}
           </span>
         </div>
-        <div className='flex items-center gap-2'>
-          <CalendarDays className='w-4 h-4 text-purple-600' />
+        <div className="flex items-center gap-2">
+          <CalendarDays className="w-4 h-4 text-purple-600" />
           <span>
             <strong>Session:</strong> {result.session}
           </span>
         </div>
-        <div className='flex items-center gap-2'>
-          <CalendarDays className='w-4 h-4 text-purple-600' />
+        <div className="flex items-center gap-2">
+          <CalendarDays className="w-4 h-4 text-purple-600" />
           <span>
             <strong>Term:</strong> {result.term}
           </span>
         </div>
-      
-        
-        <div className='flex items-center gap-2'>
-          <LineChart className='w-4 h-4 text-blue-600' />
+
+        <div className="flex items-center gap-2">
+          <LineChart className="w-4 h-4 text-blue-600" />
           <span>
-            <strong>Average:</strong> {result.averageScore.toString().slice(0, 5)}%
+            <strong>Total Score:</strong> {result.totalScore}
           </span>
         </div>
-        <div className='flex items-center gap-2'>
-          <Users className='w-4 h-4 text-green-600' />
+        <div className="flex items-center gap-2">
+          <LineChart className="w-4 h-4 text-blue-600" />
+          <span>
+            <strong>Average:</strong>{" "}
+            {result.averageScore.toString().slice(0, 5)}%
+          </span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Users className="w-4 h-4 text-green-600" />
           <span>
             <strong>Class Size:</strong> {result.numberInClass}
           </span>
